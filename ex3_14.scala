d42ea624cbaf5964c378fc6be807f8169f7a8863
Andy Stanton <andystanton@gmail.com>
2015-10-06 15:34:46 +0100

    more readable ex3_14

--- ex3_14.scala
+++ ex3_14.scala
@@ -1,9 +1,9 @@
 object Ex3_14 {
   def append[A](as: List[A], a: A): List[A] = {
-    as.foldLeft(List[A](a))((a, b) => a match {
+    as.foldLeft(List(a))((a, b) => a match {
         case h :: t => h :: b :: t
         case _ => Nil
-      }).foldLeft(List[A]())((a, b) => (b :: a))
+      }).foldLeft(Nil: List[A])((a, b) => (b :: a))
   }
 
   def main(args: Array[String]) {
