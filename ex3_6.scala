c682d6d4701ea193cc845115b41ade584488ad5a
Andy Stanton <andystanton@gmail.com>
2015-10-28 17:55:38 +0000

    use reverse at the end instead of list append

--- ex3_6.scala
+++ ex3_6.scala
@@ -2,12 +2,16 @@ object Ex3_6 {
   def init[A](l: List[A]): List[A] = {
     @annotation.tailrec
     def loop(l: List[A], build: List[A]): List[A] = l match {
-      case h :: t if t.length > 1 => loop(t, build ++ List(h))
-      case h :: t if t != Nil => build ++ List(h)
-      case _ => Nil
+      case h :: t if l.length > 1 => loop(t, h :: build)
+      case _ => build
+    }
+    
+    def reverse(l: List[A], build: List[A]): List[A] = l match {
+      case h :: t => reverse(t, h :: build)
+      case _ => build
     }
 
-    loop(l, Nil)
+    reverse(loop(l, Nil), Nil)
   }
 
   def main(args: Array[String]) {
