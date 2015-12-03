6cea6928dc60071beb09fe810ecee15e490c3c04
andystanton <andystanton@gmail.com>
2015-12-03 21:33:07 +0000

    exercise 3.26 and 3.27

--- src/main/scala/fpinscala/datastructures/Tree.scala
+++ src/main/scala/fpinscala/datastructures/Tree.scala
@@ -10,4 +10,19 @@ object Tree {
     case Branch(left, right) => 1 + size(left) + size(right)
     case Leaf(_) => 1
   }
+
+  // exercise 3.26
+  def maximum(tree: Tree[Int]): Int = tree match {
+    case Branch(left, right) => maximum(left).max(maximum(right))
+    case Leaf(value) => value
+  }
+
+  // exercise 3.27
+  def depth[A](tree: Tree[A]): Int = {
+    def loop(tree: Tree[A], carry: Int): Int = tree match {
+      case Branch(left, right) => loop(left, carry + 1).max(loop(right, carry + 1))
+      case Leaf(_) => carry + 1
+    }
+    loop(tree, 0)
+  }
 }
--- src/test/scala/fpinscala/datastructures/TreeSpec.scala
+++ src/test/scala/fpinscala/datastructures/TreeSpec.scala
@@ -9,4 +9,18 @@ class TreeSpec extends FlatSpec with Matchers {
     Tree.size(Branch(Leaf(), Leaf())) shouldBe 3
     Tree.size(Branch(Branch(Leaf(), Leaf()), Branch(Leaf(), Leaf()))) shouldBe 7
   }
+
+  // exercise 3.26
+  it should "find the maximum element in a tree of integers" in {
+    Tree.maximum(Leaf[Int](3)) shouldBe 3
+    Tree.maximum(Branch(Leaf(2), Leaf(3))) shouldBe 3
+    Tree.maximum(Branch(Branch(Leaf(-2), Leaf(8)), Leaf(12))) shouldBe 12
+  }
+
+  // exercise 3.27
+  it should "find the depth of a tree" in {
+    Tree.depth(Leaf()) shouldBe 1
+    Tree.depth(Branch(Leaf(), Leaf())) shouldBe 2
+    Tree.depth(Branch(Branch(Leaf(), Leaf()), Branch(Branch(Leaf(), Leaf()), Leaf()))) shouldBe 4
+  }
 }
