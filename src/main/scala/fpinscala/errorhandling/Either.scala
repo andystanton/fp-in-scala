3645375479e483e4b7ed8e8c495d107c6f8f552f
Andy Stanton <andystanton@gmail.com>
2015-12-09 09:03:11 +0000

    exercise 4.6

--- src/main/scala/fpinscala/errorhandling/Either.scala
+++ src/main/scala/fpinscala/errorhandling/Either.scala
@@ -6,8 +6,22 @@ case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
 
 sealed trait Either[+E, +A] {
-  def map[B](f: A => B): Either[E, B] = ???
-  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+  // exercise 4.6
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Left(error) => Left(error)
+    case Right(value) => Right(f(value))
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(error) => Left(error)
+    case Right(value) => f(value)
+  }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(_) => b
+    case Right(_) => this
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    flatMap(ia => b.map(ib => f(ia, ib)))
 }
--- src/test/scala/fpinscala/errorhandling/EitherSpec.scala
+++ src/test/scala/fpinscala/errorhandling/EitherSpec.scala
@@ -4,4 +4,33 @@ import fpinscala.datastructures._
 import org.scalatest._
 
 class EitherSpec extends FlatSpec with Matchers {
+  // exercise 4.6
+  "An Either" should "map its value" in {
+    val first: Either[String, Int] = Left("no")
+    val second: Either[String, Int] = Right(3)
+    first.map(_.toString) shouldBe Left("no")
+    second.map(_.toString) shouldBe Right("3")
+  }
+
+  it should "return a sensible default" in {
+    Right(3).orElse(Right(2)) shouldBe Right(3)
+    Left("no").orElse(Right(2)) shouldBe Right(2)
+  }
+
+  it should "flatMap its value" in {
+    val first: Either[String, Int] = Left("no")
+    val second: Either[String, Int] = Right(3)
+    first.flatMap(x => Left(x.toString)) shouldBe Left("no")
+    second.flatMap(x => Right(x.toString)) shouldBe Right("3")
+  }
+
+  it should "map apply a function to itself and another Either" in {
+    val first: Either[String, Int] = Right(4)
+    val second: Either[String, Int] = Right(3)
+    first.map2(second)(_*_) shouldBe Right(12)
+
+    val third: Either[String, Int] = Left("no")
+    val fourth: Either[String, Int] = Right(3)
+    third.map2(fourth)(_*_) shouldBe Left("no")
+  }
 }
