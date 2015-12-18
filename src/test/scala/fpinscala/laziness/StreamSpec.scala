2c27a61959e4aad6faf942816a9ae6a871bb64a3
Andy Stanton <andystanton@gmail.com>
2015-12-18 17:44:09 +0000

    exercise 5.1

--- src/test/scala/fpinscala/laziness/StreamSpec.scala
+++ src/test/scala/fpinscala/laziness/StreamSpec.scala
@@ -3,5 +3,9 @@ package fpinscala.laziness
 import org.scalatest._
 
 class StreamSpec extends FlatSpec with Matchers {
-
+  // exercise 5.1
+  "A Stream" should "be converted to a List" in {
+    val foo = Stream(1, 2, 3, 4, 5)
+    foo.toList shouldBe List(1, 2, 3, 4, 5)
+  }
 }
