4f9c0570ed26cf84047de5fed6657e5351ef9f6e
Andy Stanton <andystanton@gmail.com>
2016-01-06 18:54:21 +0000

    Add recursive solution to exercise 5.1
    Add solution to exercise 5.2

--- src/main/scala/fpinscala/laziness/Stream.scala
+++ src/main/scala/fpinscala/laziness/Stream.scala
@@ -6,12 +6,64 @@ sealed trait Stream[+A] {
     case Cons(h, t) => Some(h())
   }
 
+  override def toString: String = this match {
+    case Empty => "Empty"
+    case Cons(h, t) => s"Cons(${h()}, ${t().toString})"
+  }
+
+  def ===(other: Stream[Any]): Boolean = other match {
+    case sameType: Stream[A] => this match {
+      case Cons(h, t) => other match {
+        case Cons(oh, ot) => h() == oh() && t() === ot()
+        case _ => false
+      }
+      case _ => other == Empty
+    }
+    case _ => false
+  }
+
+  def reverse: Stream[A] = {
+    def loop(as: Stream[A], out: Stream[A]): Stream[A] = as match {
+      case Cons(h, t) => loop(t(), Cons(h, () => out))
+      case _ => out
+    }
+    loop(this, Empty)
+  }
+
   // exercise 5.1
   def toList: List[A] = this match {
     case Empty => Nil
     case Cons(h, t) => h() :: t().toList
   }
+
+  def toListViaRecursion: List[A] = {
+    @annotation.tailrec
+    def loop(l: List[A], s: Stream[A]): List[A] = s match {
+      case Empty => l
+      case Cons(h, t) => loop(h() :: l, t())
+    }
+    loop(Nil: List[A], this).reverse
+  }
+
+  // exercise 5.2
+  def take(n: Int): Stream[A] = {
+    def loop(n: Int, out: Stream[A], as: Stream[A]): Stream[A] = as match {
+      case Cons(h, t) if n > 0 => loop(n - 1, Cons(h, () => out), t())
+      case _ => out
+    }
+    loop(n, Empty, this).reverse
+  }
+
+  def drop(n: Int): Stream[A] = {
+    def loop(n: Int, out: Stream[A], as: Stream[A]): Stream[A] = as match {
+      case Cons(h, t) if n > 0 => loop(n - 1, out, t())
+      case Cons(h, t) => loop(n - 1, Cons(h, () => out), t())
+      case _ => out
+    }
+    loop(n, Empty, this).reverse
+  }
 }
+
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
--- src/test/scala/fpinscala/laziness/StreamSpec.scala
+++ src/test/scala/fpinscala/laziness/StreamSpec.scala
@@ -3,9 +3,43 @@ package fpinscala.laziness
 import org.scalatest._
 
 class StreamSpec extends FlatSpec with Matchers {
+  def shouldBeSameStream(a: Stream[Any], b: Stream[Any]) = a === b
+
   // exercise 5.1
   "A Stream" should "be converted to a List" in {
     val foo = Stream(1, 2, 3, 4, 5)
     foo.toList shouldBe List(1, 2, 3, 4, 5)
+  }
+
+  it should "be converted to a List via recursion" in {
+    val foo = Stream(1, 2, 3, 4, 5)
+    foo.toListViaRecursion shouldBe List(1, 2, 3, 4, 5)
+  }
+
+  it should "be comparable to another Stream" in {
+    // same type & equal
+    Stream() === Stream() shouldBe true
+    Stream(1) === Stream(1) shouldBe true
+    Stream(1, 2, 3, 4, 5) === Stream(1, 2, 3, 4, 5) shouldBe true
+
+    // same type & not equal
+    Stream() === Stream(1) shouldBe false
+    Stream() === Stream(1, 2, 3, 4, 5) shouldBe false
+
+    // not same type
+    Stream(1) === Stream("1") shouldBe false
+  }
+
+  it should "reverse a Stream" in {
+    Stream(1, 2, 3, 4, 5).reverse === Stream(5, 4, 3, 2, 1) shouldBe true
+  }
+
+  // exercise 5.2
+  it should "take the first n elements" in {
+    Stream(1, 2, 3, 4, 5).take(3) === Stream(1, 2, 3) shouldBe true
+  }
+
+  it should "drop the first n elements" in {
+    Stream(1, 2, 3, 4, 5).drop(3) === Stream(4, 5) shouldBe true
   }
 }
