1d892ee4473b5c91d1f8f022bad4f5494add0c4d
Andy Stanton <andystanton@gmail.com>
2016-02-04 17:55:15 +0000

    move some stream tests into companion object spec

--- src/test/scala/fpinscala/laziness/StreamSpec.scala
+++ src/test/scala/fpinscala/laziness/StreamSpec.scala
@@ -73,11 +73,12 @@ class StreamSpec extends FlatSpec with Matchers {
 
   // exercise 5.5
   it should "take the first n elements matching a predicate via foldRight" in {
-    Stream(1, 2, 3, 4, 5).takeWhileViaFoldRight(_ < 4) === Stream(1, 2, 3) shouldBe true
+    Stream(1, 2, 3, 4, 5).takeWhileViaFoldRight(_ < 4) ===
+      Stream(1, 2, 3) shouldBe true
   }
 
   // exercise 5.6
-  it should "return an Option of the first item in the stream via foldRight" in {
+  it should "return an Option of the first item via foldRight" in {
     Stream(1, 2, 3, 4, 5).headOptionViaFoldRight shouldBe Some(1)
     Stream.empty.headOptionViaFoldRight shouldBe None
   }
@@ -93,12 +94,14 @@ class StreamSpec extends FlatSpec with Matchers {
   }
 
   it should "append another Stream" in {
-    Stream(1, 2, 3, 4, 5).append(Stream(6, 7, 8, 9, 10)) === Stream(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) shouldBe true
+    Stream(1, 2, 3, 4, 5).append(Stream(6, 7, 8, 9, 10)) ===
+      Stream(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) shouldBe true
     Stream.empty[Int].append(Stream(8)) === Stream(8) shouldBe true
   }
 
   it should "flatMap over its elements" in {
-    Stream(1, 2, 3, 4, 5).flatMap(i => Stream(i, i)) === Stream(1, 1, 2, 2, 3, 3, 4, 4, 5, 5) shouldBe true
+    Stream(1, 2, 3, 4, 5).flatMap(i => Stream(i, i)) ===
+      Stream(1, 1, 2, 2, 3, 3, 4, 4, 5, 5) shouldBe true
   }
 
   it should "find an element" in {
@@ -106,54 +109,10 @@ class StreamSpec extends FlatSpec with Matchers {
     Stream(1, 2, 3, 4, 5).find(_ == 10) shouldBe None
   }
 
-  it should "return an infinite stream of ones" in {
-    Stream.ones.take(5) === Stream(1, 1, 1, 1, 1) shouldBe true
-  }
-
-  // exercise 5.8
-  it should "return an infinite stream of a constant" in {
-    Stream.constant(1).take(5) === Stream(1, 1, 1, 1, 1) shouldBe true
-  }
-
-  // exercise 5.9
-  it should "generate an infinite of integers" in {
-    Stream.from(4).take(5) === Stream(4, 5, 6, 7, 8) shouldBe true
-  }
-
-  // exercise 5.10
-  it should "generate the Fibonacci sequence" in {
-    Stream.fibs.take(10) === Stream(0, 1, 1, 2, 3, 5, 8, 13, 21, 34) shouldBe true
-  }
-
-  // exercise 5.11
-  it should "construct Streams in a general way" in {
-    Stream.unfold(3)(carried =>
-      Some((carried, carried + 2))).take(5) === Stream(3, 5, 7, 9, 11) shouldBe true
-
-    Stream.unfold(3)(carried =>
-      if (carried > 5) None else Some((carried, carried + 1))).take(5) === Stream(3, 4, 5) shouldBe true
-  }
-
-  // exercise 5.12
-  it should "return an infinite stream of ones via unfold" in {
-    Stream.onesViaUnfold.take(5) === Stream(1, 1, 1, 1, 1) shouldBe true
-  }
-
-  it should "return an infinite stream of a constant via unfold" in {
-    Stream.constantViaUnfold(1).take(5) === Stream(1, 1, 1, 1, 1) shouldBe true
-  }
-
-  it should "generate an infinite of integers via unfold" in {
-    Stream.fromViaUnfold(4).take(5) === Stream(4, 5, 6, 7, 8) shouldBe true
-  }
-
-  it should "generate the Fibonacci sequence via unfold" in {
-    Stream.fibsViaUnfold.take(10) === Stream(0, 1, 1, 2, 3, 5, 8, 13, 21, 34) shouldBe true
-  }
-
   // exercise 5.13
   it should "map over its elements via unfold" in {
-    Stream(1, 2, 3, 4, 5).mapViaUnfold(_ * 2) === Stream(2, 4, 6, 8, 10) shouldBe true
+    Stream(1, 2, 3, 4, 5).mapViaUnfold(_ * 2) ===
+      Stream(2, 4, 6, 8, 10) shouldBe true
   }
 
   it should "take the first n elements via unfold" in {
@@ -161,7 +120,8 @@ class StreamSpec extends FlatSpec with Matchers {
   }
 
   it should "take the first n elements matching a predicate via unfold" in {
-    Stream(1, 2, 3, 4, 5).takeWhileViaUnfold(_ < 4) === Stream(1, 2, 3) shouldBe true
+    Stream(1, 2, 3, 4, 5).takeWhileViaUnfold(_ < 4) ===
+      Stream(1, 2, 3) shouldBe true
   }
 
   it should "zip two streams together via unfold" in {
@@ -189,5 +149,54 @@ class StreamSpec extends FlatSpec with Matchers {
       (Some(7), None),
       (Some(8), None),
       (Some(9), None)) shouldBe true
+  }
+
+  "A Stream companion object" should "return an infinite stream of ones" in {
+    Stream.ones.take(5) === Stream(1, 1, 1, 1, 1) shouldBe true
+  }
+
+  // exercise 5.8
+  it should "return an infinite stream of a constant" in {
+    Stream.constant(1).take(5) === Stream(1, 1, 1, 1, 1) shouldBe true
+  }
+
+  // exercise 5.9
+  it should "generate an infinite of integers" in {
+    Stream.from(4).take(5) === Stream(4, 5, 6, 7, 8) shouldBe true
+  }
+
+  // exercise 5.10
+  it should "generate the Fibonacci sequence" in {
+    Stream.fibs.take(10) ===
+      Stream(0, 1, 1, 2, 3, 5, 8, 13, 21, 34) shouldBe true
+  }
+
+  // exercise 5.11
+  it should "construct Streams in a general way" in {
+    Stream.unfold(3)(carried =>
+      Some((carried, carried + 2))).take(5) ===
+      Stream(3, 5, 7, 9, 11) shouldBe true
+
+    Stream.unfold(3)(carried =>
+      if (carried > 5) None else Some((carried, carried + 1))).take(5) ===
+      Stream(3, 4, 5) shouldBe true
+  }
+
+  // exercise 5.12
+  it should "return an infinite stream of ones via unfold" in {
+    Stream.onesViaUnfold.take(5) === Stream(1, 1, 1, 1, 1) shouldBe true
+  }
+
+  it should "return an infinite stream of a constant via unfold" in {
+    Stream.constantViaUnfold(1).take(5) === Stream(1, 1, 1, 1, 1) shouldBe true
+  }
+
+  it should "generate an infinite of integers via unfold" in {
+    Stream.fromViaUnfold(4).take(5) === Stream(4, 5, 6, 7, 8) shouldBe true
+  }
+
+  it should "generate the Fibonacci sequence via unfold" in {
+    Stream.fibsViaUnfold.take(10) ===
+      Stream(0, 1, 1, 2, 3, 5, 8, 13, 21, 34) shouldBe true
   }
 }
