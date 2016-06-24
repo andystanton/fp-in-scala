8737746a2f96bbf59ce2f74502d93080af9e5ac9
Andy Stanton <andystanton@gmail.com>
2016-06-24 19:01:11 +0100

    exercise 7.1

---
+++ src/main/scala/fpinscala/parallelism/Par.scala
@@ -0,0 +1,13 @@
+package fpinscala.parallelism
+
+case class Par[A]()
+
+object Par {
+  def unit[A](a: => A): Par[A] = ???
+  def get[A](a: Par[A]): A = ???
+
+  // exercise 7.1
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = ???
+
+  def fork[A](a: => Par[A]): Par[A] = ???
+}
