555da5108c4f5292a86d1786a24023c99388ea40
Andy Stanton <andystanton@gmail.com>
2016-06-24 09:18:05 +0100

    Exercise 6.11

--- src/main/scala/fpinscala/functionalstate/CandyMachine.scala
+++ src/main/scala/fpinscala/functionalstate/CandyMachine.scala
@@ -1,11 +1,32 @@
 package fpinscala.functionalstate
 
 sealed trait Input
+
 case object Coin extends Input
+
 case object Turn extends Input
 
 case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object Machine {
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+  type MachineState = State[Machine, (Int, Int)]
+
+  // exercise 6.11
+  def simulateMachine(inputs: List[Input]): MachineState = inputs match {
+    case Nil => throw new IllegalArgumentException("Cannot simulate machine with no inputs")
+    case _ => State.sequence(inputs.map {
+      case Coin => new MachineState({
+        case Machine(true, candies, coins) if candies > 0 =>
+          ((coins + 1, candies), Machine(locked = false, candies, coins + 1))
+        case machine =>
+          ((machine.coins, machine.candies), machine)
+      })
+      case Turn => new MachineState({
+        case machine@Machine(true, candies, coins) =>
+          ((coins, candies), machine)
+        case Machine(_, candies, coins) =>
+          ((coins, candies - 1), Machine(locked = true, candies - 1, coins))
+      })
+    }).map(_.last)
+  }
 }
--- src/test/scala/fpinscala/functionalstate/CandyMachineSpec.scala
+++ src/test/scala/fpinscala/functionalstate/CandyMachineSpec.scala
@@ -3,6 +3,7 @@ package fpinscala.functionalstate
 import org.scalatest.{FlatSpec, Matchers}
 
 class CandyMachineSpec extends FlatSpec with Matchers {
+  // exercise 6.11
   "A Candy Machine" should "work" in {
     val machine = Machine(locked = true, candies = 5, coins = 10)
     val ((resultCoins, resultCandies), _) = Machine.simulateMachine(List(
@@ -18,5 +19,68 @@ class CandyMachineSpec extends FlatSpec with Matchers {
 
     resultCoins shouldBe 14
     resultCandies shouldBe 1
+  }
+
+  it should "unlock when inserting a coin if it is locked and there are candies left" in {
+    val machine = Machine(locked = true, candies = 5, coins = 10)
+    val ((resultCoins, resultCandies), outMachine) = Machine.simulateMachine(List(Coin)).run(machine)
+
+    resultCoins shouldBe 11
+    resultCandies shouldBe 5
+    outMachine.locked shouldBe false
+  }
+
+  it should "not unlock when inserting a coin if it is locked and there are no candies left" in {
+    val machine = Machine(locked = true, candies = 0, coins = 10)
+    val ((resultCoins, resultCandies), outMachine) = Machine.simulateMachine(List(Coin)).run(machine)
+
+    resultCoins shouldBe 10
+    resultCandies shouldBe 0
+    outMachine.locked shouldBe true
+  }
+
+  it should "dispense candy and become locked when it is turned if it is unlocked" in {
+    val machine = Machine(locked = false, candies = 8, coins = 10)
+    val ((resultCoins, resultCandies), outMachine) = Machine.simulateMachine(List(Turn)).run(machine)
+
+    resultCoins shouldBe 10
+    resultCandies shouldBe 7
+    outMachine.locked shouldBe true
+  }
+
+  it should "do nothing when it is turned on a locked machine" in {
+    val machine = Machine(locked = true, candies = 8, coins = 10)
+    val ((resultCoins, resultCandies), outMachine) = Machine.simulateMachine(List(Turn)).run(machine)
+
+    resultCoins shouldBe 10
+    resultCandies shouldBe 8
+    outMachine.locked shouldBe true
+  }
+
+  it should "do nothing when inserting a coin on a unlocked machine" in {
+    val machine = Machine(locked = false, candies = 8, coins = 10)
+    val ((resultCoins, resultCandies), outMachine) = Machine.simulateMachine(List(Coin)).run(machine)
+
+    resultCoins shouldBe 10
+    resultCandies shouldBe 8
+    outMachine.locked shouldBe false
+  }
+
+  it should "do nothing if there is no candy" in {
+    val machine = Machine(locked = true, candies = 0, coins = 10)
+    val ((resultCoins, resultCandies), outMachine) = Machine.simulateMachine(List(Coin)).run(machine)
+
+    resultCoins shouldBe 10
+    resultCandies shouldBe 0
+    outMachine.locked shouldBe true
+  }
+
+  it should "handle being run with no inputs" in {
+    val machine = Machine(locked = true, candies = 0, coins = 10)
+
+    intercept[IllegalArgumentException] {
+      val machine = Machine(locked = true, candies = 6, coins = 10)
+      Machine.simulateMachine(Nil: List[Input]).run(machine)
+    }.getMessage shouldBe "Cannot simulate machine with no inputs"
   }
 }
